c1b5197e8bda65f166a4f06bc135ab970c7ab855
Branimir Makovic <branimir.markovic@ezderm.com>
2022-11-22 10:51:48 +0100

    Comments

---
+++ Comments.swift
@@ -0,0 +1,18 @@
+//
+//  Comments.swift
+//  Clean Code Study
+//
+//  Created by Branimir Markovic on 17.11.22..
+//
+
+import Foundation
+
+// MARK: - Comments
+
+// “Nothing can be quite so helpful as a well-placed comment. Nothing can clutter up a module more than frivolous dogmatic comments.“Nothing can be quite so damaging as an old crufty comment that propagates lies and misinformation.”
+
+
+
+
+
+
